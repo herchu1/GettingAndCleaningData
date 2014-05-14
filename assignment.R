7c636264249efae75def35b2a504bd26846015f3
herchu1 <hfoffani+datasciencecoursera@gmail.com>
2014-05-14 16:20:14 +0200

    Va script.

---
+++ assignment.R
@@ -0,0 +1,34 @@
+
+# setwd('OneDrive/Practicas/DS-CLEAN')
+colnames <- read.table('UCi HAR Dataset/features.txt')
+names(colnames) = c('n','name')
+nnames <- gsub("[[:punct:]]", "", colnames$name)
+nnames <- tolower(nnames)
+
+nnames <- tolower(nnames)
+nnames <- gsub("^t", "time", nnames)
+nnames <- gsub("^f", "fft", nnames)
+
+grep("mean", nnames, fixed=T)
+grep("std", nnames, fixed=T)
+st <- grepl("std", nnames, fixed=T)
+mn <- grepl("mean", nnames, fixed=T)
+mnst <- st | mn
+
+x <- read.table('UCi HAR Dataset/train/X_train.txt')
+
+xf <- x[,mnst]
+names(xf) <- nnames[mnst]
+
+
+subjecttrain <- read.table('UCi HAR Dataset/train/subject_train.txt')
+names(subjecttrain) <- "subject"
+
+activities <- read.table('UCi HAR Dataset/activity_labels.txt')
+
+y_train <- read.table('UCi HAR Dataset/train/y_train.txt')
+activitytrain <- cut(y_train[[1]], 6, labels=activities[[2]])
+
+train <- cbind(xf, subjecttrain, activity=activitytrain)
+
+
