a1ef5de49490253441af2eeeaa71cf1e528d2c0d
herchu1 <hfoffani+datasciencecoursera@gmail.com>
2014-05-25 17:08:25 +0200

    More comments.

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,53 @@
+#
+# run_analysis.R
+#
+# author:   herchu
+#           for Coursera's Getting And Cleaning Data course (May 2014).
+#
+# process raw Human Activity Recognition Using Smartphones Dataset.
+# Source of the data:
+# Human Activity Recognition on Smartphones using a Multiclass
+# Hardware-Friendly Support Vector Machine.
+# International Workshop of Ambient Assisted Living (IWAAL 2012).
+# Vitoria-Gasteiz, Spain. Dec 2012
 
 
-# 
-writetidydata <- function(topdirectory, filename='tidydata.txt') {
+writetidydata <- function(topdirectory='UCI HAR Dataset', filename='tidydata.txt') {
+    # process the raw data and writes an aggregated and tidy data set.
+    #
+    # parameters:
+    #   topdirectory: the path of the directory where the uncompress raw data is.
+    #                 defaults to 'UCI HAR Dataset' in the current working dir.
+    #   filename:     the name of the output file.
+    #                 defaults to 'tidydata.txt' in the curring working dir.
+    #
     ds <- obtaindataset(topdirectory)
-    writedata(ds, filename)
+    writeaggregatedata(ds, filename)
 }
 
-writedata <- function(alldata, filename) {
+
+writeaggregatedata <- function(alldata, filename) {
+    # aggregates the full data set with the average of all the columns
+    # by subject and activity.
+    #
+    # parameters:
+    #   alldata:      a tidy data set with the full data.
+    #   filename:     the name of the output file.
+    #                 defaults to 'tidydata.txt' in the curring working dir.
+    #
     compact <- aggregate( .~ subject + activity, data=alldata, mean)
     write.csv(compact, file=filename, row.names=F)
 }
 
-obtaindataset <- function(topdirectory) {
 
+obtaindataset <- function(topdirectory) {
+    # obtains the full data set by reading and cleaning the raw data.
+    #
+    # parameters:
+    #   topdirectory: the path of the directory where the uncompress raw data is.
+    # returns:
+    #   a tidy data set
+    #
     filepath <- paste(topdirectory, '/features.txt', sep='')
     colnames <- read.table(filepath)
     names(colnames) = c('n','name')
@@ -40,7 +75,17 @@ obtaindataset <- function(topdirectory) {
 
 
 obtainsubset <- function(topdir, subset, activities, columnset, newnames) {
-    
+    # obtains a subset (either train or test) of the data data.
+    #
+    # parameters:
+    #     topdir:       the path of the directory where the uncompress raw data is.
+    #     subset:       either 'train' or 'test'.
+    #     activities:   the activities vector for the corresponding subset.
+    #     columnset:    the subset of columns to extract.
+    #     newnames:     the new names of the columns.
+    # returns:
+    #     a tidy data subset (either train or test)
+    #
     filepath <- paste(topdir, '/', subset, '/X_', subset, '.txt', sep='')
     x <- read.table(filepath)
     
